c31dde50e0f2fb74610749ef629ad0b43da6fbe6
ZeronSL <zeron_n@outlook.com>
2025-08-09 16:31:32 +0700

    testing

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,11 @@
-import { Button, Container, Typography } from '@mui/material';
+import { Button, Container, Toolbar, Typography } from '@mui/material';
 
 export default function Home() {
   return (
-    <Container sx={{ textAlign: 'center', paddingTop: 5 }}>
+    <Container sx={{ textAlign: 'center', paddingTop: 5 , maxWidth:"sm", backgroundColor:"LightGrey"}}>
+        <Toolbar disableGutters>
+
+        </Toolbar>
       <Typography variant="h3" gutterBottom>
         Welcome to My MUI App
       </Typography>
