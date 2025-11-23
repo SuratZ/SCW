b760c6623906b1b6b33d3f5b7fde9650003f3ec5
SuratZ <Zeron_N@outlook.com>
2025-11-24 02:03:50 +0700

    update refresh version

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'inject-version-query',
+      transformIndexHtml(html: string) {
+        const version: number = Date.now();
+        return html
+          .replaceAll('.js"',`.js?v=${version}"`)
+          .replaceAll('.css"',`.css?v=${version}"`);
+      }
+    }
+  ],
   base: '/scw/',  // 👈 important!
 })
